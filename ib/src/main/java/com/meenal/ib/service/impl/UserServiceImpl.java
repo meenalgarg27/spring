9f2ea732c0a70e80415c81eaa8826eac36ce4303
meenalgarg27 <meenalgarg27@gmail.com>
2019-04-27 13:00:24 -0300

    updating service

--- ib/src/main/java/com/meenal/ib/service/impl/UserServiceImpl.java
+++ ib/src/main/java/com/meenal/ib/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.meenal.ib.service.impl;
 
+import com.meenal.ib.dao.GovernmentIdDao;
 import com.meenal.ib.dao.UserDao;
 import com.meenal.ib.service.UserService;
 
@@ -8,14 +9,17 @@ import user.meenal.ib.model.User;
 public class UserServiceImpl implements UserService{
 
 	private final UserDao userDao;
+	private final GovernmentIdDao governmentIdDao;
 
-	public UserServiceImpl(UserDao userDao) {
+	public UserServiceImpl(UserDao userDao, GovernmentIdDao governmentIdDao) {
 		super();
 		this.userDao = userDao;
+		this.governmentIdDao = governmentIdDao;
 	}
 	@Override
 	public void registerUser(User user) {
 
+		
 		
 		
 //		this.userDao.save(user);
